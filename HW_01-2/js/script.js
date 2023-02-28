742978b602991af829924b29a8de4b6eb79d145d
Maksym Che <maksymchernysh911@gmail.com>
2023-02-28 17:26:33 +0200

    transmission-1

---
+++ HW_01-2/js/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Можливо не вірно зрозумів задачу, (там не зовсім однозначно сказано), 
+// якщо виводимо в консоль лише результат, то ось код:
+
+
+// const a = +prompt('Введіть 1-ше число', '5');
+// console.log(a);
+// const b = +prompt('Введіть 2-ге число', '34');
+// console.log(b);
+
+// const plus = a + b;
+// console.log(plus);
+// const minus = a - b;
+// console.log(minus);
+// const mult = a * b;
+// console.log(mult);
+// const divi = a / b;
+// console.log(divi);
+
+
+
+const a = +prompt('Введіть 1-ше число', '52');
+console.log(a);
+const b = +prompt('Введіть 2-ге число', '4');
+console.log(b);
+alert('Сума чисел буде = ' + (a + b));
+
+const c = +prompt('Введіть 1-ше число', '50');
+console.log(a);
+const d = +prompt('Введіть 2-ге число', '5');
+console.log(b);
+alert('Різниц чисел буде = ' + (c - d));
+
+const e = +prompt('Введіть 1-ше число', '50');
+console.log(a);
+const f = +prompt('Введіть 2-ге число', '5');
+console.log(b);
+alert('Множення чисел буде = ' + (e * f));
+
+const g = +prompt('Введіть 1-ше число', '50');
+console.log(a);
+const h = +prompt('Введіть 2-ге число', '5');
+console.log(b);
+alert('Ділення чисел дасть результат - ' + (g / h));
+
+
+
+
+
