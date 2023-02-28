77aa719d16780512f5fb7a4da5ebd72b0b47c3f1
Maksym Che <maksymchernysh911@gmail.com>
2023-02-28 18:13:50 +0200

    update hw-1-2

--- HW_01-2/js/script.js
+++ HW_01-2/js/script.js
@@ -1,48 +1,45 @@
 'use strict';
 
-// Можливо не вірно зрозумів задачу, (там не зовсім однозначно сказано), 
-// якщо виводимо в консоль лише результат, то ось код:
-
-
-// const a = +prompt('Введіть 1-ше число', '5');
-// console.log(a);
-// const b = +prompt('Введіть 2-ге число', '34');
-// console.log(b);
-
-// const plus = a + b;
-// console.log(plus);
-// const minus = a - b;
-// console.log(minus);
-// const mult = a * b;
-// console.log(mult);
-// const divi = a / b;
-// console.log(divi);
-
-
-
 const a = +prompt('Введіть 1-ше число', '52');
 console.log(a);
 const b = +prompt('Введіть 2-ге число', '4');
 console.log(b);
-alert('Сума чисел буде = ' + (a + b));
 
-const c = +prompt('Введіть 1-ше число', '50');
-console.log(a);
-const d = +prompt('Введіть 2-ге число', '5');
-console.log(b);
-alert('Різниц чисел буде = ' + (c - d));
+function showSum() {
+    alert('Сума чисел буде = ' + (a + b) );
+}
+showSum();
 
-const e = +prompt('Введіть 1-ше число', '50');
-console.log(a);
-const f = +prompt('Введіть 2-ге число', '5');
-console.log(b);
-alert('Множення чисел буде = ' + (e * f));
+const c = +prompt('Введіть 1-ше число', '52');
+console.log(c);
+const d = +prompt('Введіть 2-ге число', '44');
+console.log(d);
 
-const g = +prompt('Введіть 1-ше число', '50');
-console.log(a);
-const h = +prompt('Введіть 2-ге число', '5');
-console.log(b);
-alert('Ділення чисел дасть результат - ' + (g / h));
+function showMin() {
+    alert('Різниця чисел буде = ' + (c - d) );
+}
+showMin();
+
+const e = +prompt('Введіть 1-ше число', '5');
+console.log(e);
+const f = +prompt('Введіть 2-ге число', '4');
+console.log(f);
+
+function showMult() {
+    alert('Множення чисел буде = ' + (e * f) );
+}
+showMult();
+
+const g = +prompt('Введіть 1-ше число', '52');
+console.log(g);
+const h = +prompt('Введіть 2-ге число', '8');
+console.log(h);
+
+function showDivi() {
+    alert('Ділення чисел буде = ' + (g / h) );
+}
+showDivi();
+
 
 
 
