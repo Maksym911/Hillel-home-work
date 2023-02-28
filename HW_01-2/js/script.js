b0793facbf57cf2f280f4c12410c93541e07ac9a
Maksym Che <maksymchernysh911@gmail.com>
2023-02-28 09:28:15 +0200

    hw1-2

---
+++ HW_01-2/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Можливо не вірно зрозумів задачу, (там не зовсім однозначно сказано), 
+// якщо виводимо в консоль лише результат, то ось код:
+
+
+let a = +prompt('Введіть 1-ше число', '5');
+console.log(a);
+let b = +prompt('Введіть 2-ге число', '34');
+console.log(b);
+
+let plus = a + b;
+console.log(plus);
+let minus = a - b;
+console.log(minus);
+let mult = a * b;
+console.log(mult);
+let divi = a / b;
+console.log(divi);
