de61e137bda635f93c230feffb7ccbbef574c0f1
Maksym Che <maksymchernysh911@gmail.com>
2023-03-04 00:24:38 +0200

    update

---
+++ HW_01-3/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// #1
+// let admin = null;
+// let name = null;
+// name = 'Джон';
+// admin = name;
+// alert(admin);
+
+
+
+// #2
+// let nameOurPlanet = Earth;
+// let presentUserName = 'Natasha';
+
+
+
+// #3
+let number = '10' + 10;
+console.log(number);
+console.log(typeof number);
