d4cb009787bfbaebfbcc8f22011606e8012f0987
Maksym Che <maksymchernysh911@gmail.com>
2023-03-11 20:34:03 +0200

    initial 9 task

---
+++ HW_9/scripts/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const firstN = +prompt('Введіть число', 3);
+console.log(firstN);
+const secondN = +prompt('Введіть друге число', 14);
+console.log(secondN);
+
+function foo(firstN, secondN) {
+if (firstN = secondN) {
+    return true;
+} else {
+    return false;
+    }
+}
+
+console.log(foo());
