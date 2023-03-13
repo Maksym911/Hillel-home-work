d2478aa3837d635c16ecad38a7b6d57effa93557
Maksym Che <maksymchernysh911@gmail.com>
2023-03-13 17:13:55 +0200

    rewritten code

--- HW_9/scripts/index.js
+++ HW_9/scripts/index.js
@@ -1,16 +1,25 @@
 'use strict';
 
-const firstN = +prompt('Введіть число', 3);
-console.log(firstN);
-const secondN = +prompt('Введіть друге число', 14);
-console.log(secondN);
+// #1
+// const firstN = +prompt('Введіть число', 7);
+// const secondN = +prompt('Введіть друге число', 7);
+// function foo(firstN, secondN) {
+//     return firstN === secondN;
+// }
+// console.log(foo(firstN, secondN));
 
-function foo(firstN, secondN) {
-if (firstN = secondN) {
-    return true;
-} else {
-    return false;
-    }
+// #2
+// const firstN = +prompt('Введіть число', 7);
+// const secondN = +prompt('Введіть друге число', 1);
+
+// function foo(firstN, secondN) {
+//     return (firstN + secondN) > 10;
+// }
+// console.log(foo(firstN, secondN));
+
+// #3
+const num = +prompt('Введіть будь яке число', -7);
+function foo(num) {
+    return num < 0;
 }
-
-console.log(foo());
+console.log(foo(num));
