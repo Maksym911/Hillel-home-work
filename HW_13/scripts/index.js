432747833e69c581d21c0fc31f55f04ed3a3db0d
Maksym Che <maksymchernysh911@gmail.com>
2023-03-16 02:23:04 +0200

    update for null

--- HW_13/scripts/index.js
+++ HW_13/scripts/index.js
@@ -1,16 +1,23 @@
 'use strict';
 
-const num = +prompt('Як ви гадаєте, в якому році Пастернак отримав нобелівську премію по літературі?', '1958');
+const num = prompt('Як ви гадаєте, в якому році Пастернак отримав нобелівську премію по літературі?', '1958');
 let age = 2023 - num;
 age = String(age);
-const endA = ' рік тому';
-const endB = ' роки тому';
-const endС = ' років тому';
+const endA = ' рік тому?';
+const endB = ' роки тому?';
+const endС = ' років тому?';
+console.log(num);
 
 if (!isNaN(num)) {
-    if ((num == 2023) || (num == 0) || (num < 1900) ) {
-        alert('Це було не в цьому році!');
-    } 
+    if ((num == 2023) || (num == 0)) {
+        alert('Це було не в 2023 році!');
+    } else if (num === null) {
+        alert('Від вас очікували іншого!');
+    } else if (num < 1900) {
+        alert('Пастернак тоді ще не народився!');
+    } else if (num > 2023) {
+        alert('Пастернак помер ще в 1960 році!');
+    }
         else if (age[age.length - 1] == '1') {
             alert('По вашому, це було в ' + num + ', тобто ' + age + endA);
             } else if ((+age[age.length - 1] == 2) || (+age[age.length - 1] == 3) || (+age[age.length - 1] == 4)) {
